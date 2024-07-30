d3aa70140d6f11072555726021e0e0e8cf0dd798
titaniper <flutter.kr@gmail.com>
2024-07-30 12:16:26 +0900

    Implement the record filtering and transformation features (#1)

--- app/src/main/kotlin/org/example/config/SinkConnectorConfig.kt
+++ app/src/main/kotlin/org/example/config/SinkConnectorConfig.kt
@@ -15,9 +15,14 @@ class SinkConnectorConfig(props: Map<String, String>) : AbstractConfig(CONFIG, p
         const val SINK_BOOTSTRAP_SERVER: String = "kafka.sink.bootstrap"
         const val SINK_BOOTSTRAP_SERVER_DOC: String = "Define sink bootstrap"
 
+        const val PRODUCER_MAX_REQUEST_SIZE: String = "producer.max.request.size"
+        const val PRODUCER_MAX_REQUEST_SIZE_DEFAULT: Int = 1048576 // https://docs.confluent.io/platform/current/installation/configuration/producer-configs.html#max-request-size
+        const val PRODUCER_MAX_REQUEST_SIZE_DOC: String = "producer.max.request.size"
+
         var CONFIG: ConfigDef = ConfigDef()
                 .define(SOURCE_TOPIC, ConfigDef.Type.STRING, "", Importance.HIGH, SOURCE_TOPIC_DOC)
                 .define(SINK_TOPIC, ConfigDef.Type.STRING, "", Importance.HIGH, SINK_TOPIC_DOC)
-                .define(SINK_BOOTSTRAP_SERVER, ConfigDef.Type.STRING, "", Importance.HIGH, SINK_BOOTSTRAP_SERVER_DOC) // timestamp parsing
+                .define(SINK_BOOTSTRAP_SERVER, ConfigDef.Type.STRING, "", Importance.HIGH, SINK_BOOTSTRAP_SERVER_DOC)
+                .define(PRODUCER_MAX_REQUEST_SIZE, ConfigDef.Type.INT, PRODUCER_MAX_REQUEST_SIZE_DEFAULT, Importance.LOW, PRODUCER_MAX_REQUEST_SIZE_DOC)
     }
 }
